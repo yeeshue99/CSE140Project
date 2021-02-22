82cbaac494fbf65e109fe45b50079820a6084204
yeeshue99 <35947745+yeeshue99@users.noreply.github.com>
2021-02-22 15:17:20 -0800

    Rework string returning bin to hex
    Recreate function to take in a string and return a string to bypass stoi length error for j-type instructions.

--- Lab03/Decode.cpp
+++ Lab03/Decode.cpp
@@ -10,6 +10,7 @@
 #include "RTypeInstruction.h"
 #include "ITypeInstruction.h"
 #include "JTypeInstruction.h"
+#include <unordered_map>
 
 using namespace std;
 
@@ -39,30 +40,81 @@ long int convert_binary_to_hex(long int binary)
     return stoi(hex);
 }
 
-string convert_binary_to_hex(long int binary, bool returnAsString)
+string convert_binary_to_hex(string bin)
 {
+    {
+    int l = bin.size();
+    int t = bin.find_first_of('.');
+     
+    // length of string before '.'
+    int len_left = t != -1 ? t : l;
+     
+    // add min 0's in the beginning to make
+    // left substring length divisible by 4 
+    for (int i = 1; i <= (4 - len_left % 4) % 4; i++)
+        bin = '0' + bin;
+     
+    // if decimal point exists    
+    if (t != -1)    
+    {
+        // length of string after '.'
+        int len_right = l - len_left - 1;
+         
+        // add min 0's in the end to make right
+        // substring length divisible by 4 
+        for (int i = 1; i <= (4 - len_right % 4) % 4; i++)
+            bin = bin + '0';
+    }
+     
+    // create map between binary and its
+    // equivalent hex code
+    unordered_map<string, char> bin_hex_map;
+    createMap(&bin_hex_map);
+     
+    int i = 0;
     string hex = "";
-    long int dec = 0, i = 1, remainder;
-    while (binary != 0)
+     
+    while (1)
     {
-        remainder = binary % 10;
-        dec = dec + remainder * i;
-        i = i * 2;
-        binary = binary / 10;
+        // one by one extract from left, substring
+        // of size 4 and add its hex code
+        hex += bin_hex_map[bin.substr(i, 4)];
+        i += 4;
+        if (i == bin.size())
+            break;
+             
+        // if '.' is encountered add it
+        // to result
+        if (bin.at(i) == '.')    
+        {
+            hex += '.';
+            i++;
+        }
     }
+     
+    // required hexadecimal number
+    return hex;    
+}
+}
 
-    while (dec != 0)
-    {
-        hex.insert(0, to_string(dec % 16));
-        dec = dec / 16;
-    }
-
-
-    if(hex.size() == 0){
-        hex = "0";
-    }
-
-    return hex;
+void createMap(unordered_map<string, char> *um)
+{
+    (*um)["0000"] = '0';
+    (*um)["0001"] = '1';
+    (*um)["0010"] = '2';
+    (*um)["0011"] = '3';
+    (*um)["0100"] = '4';
+    (*um)["0101"] = '5';
+    (*um)["0110"] = '6';
+    (*um)["0111"] = '7';
+    (*um)["1000"] = '8';
+    (*um)["1001"] = '9';
+    (*um)["1010"] = 'A';
+    (*um)["1011"] = 'B';
+    (*um)["1100"] = 'C';
+    (*um)["1101"] = 'D';
+    (*um)["1110"] = 'E';
+    (*um)["1111"] = 'F';
 }
 
 bool is_r_type_instruction(long int binary)
--- Lab03/Decode.h
+++ Lab03/Decode.h
@@ -2,10 +2,12 @@
 #define DECODE_H
 
 #include <map>
+#include <unordered_map>
 #include <string>
 
 long int convert_binary_to_hex(long int binary);
-std::string convert_binary_to_hex(long int binary, bool returnAsString);
+std::string convert_binary_to_hex(std::string bin);
+void createMap(std::unordered_map<std::string, char> *um);
 bool is_r_type_instruction(long int binary);
 std::map<std::string, std::string> load_instruction_file(std::string filename); 
 std::map<std::string, std::string> load_register_file(std::string filename);
